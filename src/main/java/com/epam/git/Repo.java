cb569ddb479e91c72aca80b23307926eff123267
Renata Yermak <renatkahturo@gmail.com>
2023-04-16 16:41:57 +0300

    .

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/RenataYermak/stage2-module1-git.git";
+    public static String REPO_LINK = "https://github.com/RenataYermak/new_task";
 }
