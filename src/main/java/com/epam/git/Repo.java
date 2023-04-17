1c9d146da0f526ab1eaa4cade818c6a751c282f0
Renata Yermak <renatkahturo@gmail.com>
2023-04-17 13:58:17 +0300

    .

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/RenataYermak/qertyui";
+    public static String REPO_LINK = "https://github.com/RenataYermak/new_task_git";
 }
